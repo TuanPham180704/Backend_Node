f66d3b6612e404de31fef1524df1a30a54bac931
Tuandev18 <phamtuan180704@gmail.com>
2025-09-17 22:12:13 +0700

    fs writeFileSync & readFileSync

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const http = require("node:http");
-const hostname = "localhost";
-const port  = 8080;
+const http = require('node:http')
+const hostname = 'localhost'
+const port = 8080
+const fs = require('fs')
+const path = require('path')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain'); 
-  res.end("Hello World \n");
-});
+const filePath = path.join(__dirname,'data.txt')
 
+fs.writeFileSync(filePath,"Hello Anh em")
+const content = fs.readFileSync(filePath,'utf-8')
+console.log("File content:", content);
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200
+    res.setHeader('Content-Type','text/plan')
+    res.end('Hello Wolrd \n')
+})
 
 server.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}/`)
+    console.log(`Sever is running at http://${hostname}:${port}/`);
+    
 })
