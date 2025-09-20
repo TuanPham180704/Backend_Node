26ab1bf99dc1e2dfcb651cc770cc2523716fd584
Tuandev18 <phamtuan180704@gmail.com>
2025-09-20 21:00:34 +0700

    configuration express

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const userRoutes = require('./routes/userRoutes')
-const productRoutes = require('./routes/productRoutes')
-const errorHandler = require('./middlewares/errorHandler')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const errorHandler = require("./middlewares/errorHandler");
 
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send('Hello World! 🚀')
-})
-
-
-module.exports = app
+const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World! 🚀");
+});
+app.use(errorHandler);
+module.exports = app;
